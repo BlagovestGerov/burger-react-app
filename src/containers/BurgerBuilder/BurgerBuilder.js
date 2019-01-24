0df83f9b22fb6de10b46c9b3f5e808d0b232bcc5
BlagovestGerov <blaje@abv.bg>
2019-01-24 21:19:37 +0200

    Connecting State to Build Controls

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,6 +3,15 @@ import React, { Component } from 'react';
 import Auxiliary from '../../hoc/Auxiliary';
 import Burger from '../../components/Burger/Burger';
 
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+
+const INGREDIENT_PRICES = {
+    salad: 0.5,
+    cheese: 0.4,
+    meat: 1.3,
+    bacon: 0.7
+}
+
 class BurgerBuilder extends Component{
 
         // constructor(props) {
@@ -15,15 +24,33 @@ class BurgerBuilder extends Component{
             bacon: 2,
             cheese: 2,
             meat: 1
+        },
+        tatalPrice: 4
+    };
+
+    addIngrediantHandler = (type) => {
+        const oldCount = this.state.ingredients[type];
+        const updateCounted = oldCount + 1;
+        const updateIngredients = {
+            ...this.state.ingredients
         }
+        updateIngredients[type] = updateCounted;
+        const priceAddition = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice + priceAddition;
+        this.setState({totalPrice: newPrice,ingredients: updateIngredients});
+    }
+
+    removeIngredientHandler = (type)  => {
+
     }
 
     render () {
         return (
             <Auxiliary>
                 < Burger ingredients={this.state.ingredients}/>
-                <div>Build Controls</div>
-               
+               <BuildControls 
+               ingredientAdded={this.addIngrediantHandler}/>
             </Auxiliary>
         );
     }
