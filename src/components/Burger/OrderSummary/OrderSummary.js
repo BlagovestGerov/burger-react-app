3749415c2d85c6229ba003311bb222ea893c8e32
BlagovestGerov <blaje@abv.bg>
2019-01-27 23:13:29 +0200

    Refactoring and some fixes

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,34 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-import Auxiliary from '../../../hoc/Auxiliary';
+import Auxiliary from '../../../hoc/Auxiliary/Auxiliary';
 
 import Button from '../../IU/Button/Button';
 
-const orderSummary = (props) => {
+class OrderSummary extends Component {
 
-    const ingredientSummary = Object.keys(props.ingredients)
-    .map(igKey => {
+    // This could be a functional component, doesn't have to be a class
+    componentWillUpdate() {
+        console.log('[OrderSummary]  WillUdate')
+    }
+
+    render () {
+        const ingredientSummary = Object.keys(this.props.ingredients)
+        .map(igKey => {
+            return (
+            <li key={igKey}>
+                <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {this.props.ingredients[igKey]}
+            </li>
+        )
+        });
+
         return (
-        <li key={igKey}>
-            <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
-        </li>
-    )
-    });
-    
-
-    return (
         <Auxiliary>
             <h3>Your Order</h3>
             <p>A delicious burger with the following ingredients:</p>
             <ul>
                {ingredientSummary}
             </ul>
-            <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
+            <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
             <p>Continue to Checkout?</p>
-            <Button btnType="Danger" clicked={props.purchaseCanceled}> Cancel</Button>
-            <Button btnType="Success" clicked={props.purchaseContinued}> CONTINUE</Button>
+            <Button btnType="Danger" clicked={this.props.purchaseCanceled}> Cancel</Button>
+            <Button btnType="Success" clicked={this.props.purchaseContinued}> CONTINUE</Button>
         </Auxiliary>
-    )
+        );
+    }
 };
 
-export default orderSummary;
+export default OrderSummary;
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -4,17 +4,17 @@ import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './SideDrawer.css';
 import Backdrop from '../../IU/Backdrop/Backdrop';
-import Auxiliary from '../../../hoc/Auxiliary';
+import Auxiliary from '../../../hoc/Auxiliary/Auxiliary';
 
 
 const sideDrawer = (props) => {
-    let attachedClasses = [classes.SideDrawer, classes.Closed];
+    let attachedClasses = [classes.SideDrawer, classes.Close];
     if (props.open){
         attachedClasses = [classes.SideDrawer, classes.Open];
     }
     return (
         <Auxiliary>
-            <Backdrop show={props.open} closed={props.closed}/>
+            <Backdrop show={props.open} clicked={props.closed}/>
             <div className={attachedClasses.join(' ')}>
                 <div className={classes.Logo}>
                 <Logo />
