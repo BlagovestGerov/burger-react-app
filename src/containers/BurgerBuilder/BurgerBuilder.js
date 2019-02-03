f8c39191fb1f29b625bf99793322fe1e3b8ad89e
BlagovestGerov <blaje@abv.bg>
2019-02-03 13:47:58 +0200

    Navigating to checkout page

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -104,28 +104,29 @@ class BurgerBuilder extends Component{
 
     purchaseContinueHandler = () => {
         // alert('You continue!');
-        this.setState({loading: true});
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Blagovest Gerov',
-                address: {
-                    street: 'Teststreet 1',
-                    zipCode: '1000',
-                    country: 'Bulgaria'
-                },
-                email: 'test@test.com'
-            },
-           deliveryMethod: 'fastest'
-        }
-        axios.post('/orders.json', order)
-        .then(response => 
-            this.setState({loading: false, purchasing: false})
-        )
-        .catch(error =>             
-            this.setState({loading: false, purchasing: false})
-        );        
+        // this.setState({loading: true});
+        // const order = {
+        //     ingredients: this.state.ingredients,
+        //     price: this.state.totalPrice,
+        //     customer: {
+        //         name: 'Blagovest Gerov',
+        //         address: {
+        //             street: 'Teststreet 1',
+        //             zipCode: '1000',
+        //             country: 'Bulgaria'
+        //         },
+        //         email: 'test@test.com'
+        //     },
+        //    deliveryMethod: 'fastest'
+        // }
+        // axios.post('/orders.json', order)
+        // .then(response => 
+        //     this.setState({loading: false, purchasing: false})
+        // )
+        // .catch(error =>             
+        //     this.setState({loading: false, purchasing: false})
+        // );        
+        this.props.history.push('/checkout');
     }
 
     render () {
