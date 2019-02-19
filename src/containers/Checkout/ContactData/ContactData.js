821e7accf5b63321a267e9d3b2a5b56d2cd20701
BlagovestGerov <blaje@abv.bg>
2019-02-19 17:47:54 +0200

    Added reducer and actions to the order

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -99,7 +99,6 @@ class ContactData extends Component {
                 }         
         },
         formIsValid: false,
-        loading: false
     }
 
     orderHandler = (event) => {
@@ -182,7 +181,7 @@ class ContactData extends Component {
         disabled={!this.state.formIsValid}>Order</Button>                    
         </form>
         );
-        if (this.state.loading) {
+        if (this.props.loading) {
             form = <Spinner />;
         }
         return (
@@ -197,13 +196,15 @@ class ContactData extends Component {
 const mapStateToProps = state => {
     return {
         ings: state.ingredients,
-        price: state.totalPrice
+        price: state.totalPrice,
+        loading: state.loading
     }
 }
 
 const mapDispatchToProps = dispatch => {
     return {
-        onOrderBurger: (orderData) => dispatch(actions.purchaseBurgerStart(orderData))
+        onOrderBurger: (orderData) => dispatch(actions.purchaseBurger(orderData))
+    
     }
 }
 
