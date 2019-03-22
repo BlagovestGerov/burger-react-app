c377ce6232cf931c077eacb954fe0b85291247f9
blagovest <blagovest@ever.co>
2019-03-22 18:22:19 +0200

    Added actions to the submit auth button

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
 
 import Input from '../../components/IU/Input/Input';
 
 import Button from '../../components/IU/Button/Button';
 import classes from './Auth.css';
+
+import * as actions from './../../store/actions/index';
 
 
 class Auth extends Component {
@@ -70,6 +73,11 @@ class Auth extends Component {
       
     }
 
+    submitHandler = (event) => {
+        event.preventDefault();
+        this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value)
+    }
+
     render () {
         const formElementsArray = [];
         for (let key in this.state.controls) {
@@ -93,7 +101,7 @@ class Auth extends Component {
 
         return (
             <div className={classes.Auth}>
-                <form>
+                <form onSubmit={this.submitHandler}>
                     {form}
                 <Button btnType="Success" >SUBMIT</Button>
 
@@ -103,4 +111,9 @@ class Auth extends Component {
     }
 }
 
-export default Auth;
+const mapDispatchToProps = dispatch => {
+    return {
+        onAuth: (email, password) => dispatch(actions.auth(email, password))
+    }
+}
+export default connect(null, mapDispatchToProps)(Auth);
